eca3fd108a3f183df632da4173a7f554722cb812
handrawanw <42910429+handrawanw@users.noreply.github.com>
2021-12-15 12:26:42 +0700

    Update dbConnect.js

--- dbConnect.js
+++ dbConnect.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 const dbConnect = () => {
-    // const mongoURI = process.env.MONGO_URL;
+    const mongoURI = process.env.MONGO_URL;
     mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify:false,useCreateIndex:true, connectTimeoutMS: 15000 });
     let db = mongoose.connection;
     db.on('error', console.log.bind(console, 'connection error:'));
