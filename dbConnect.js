bbc90a226e3200a4fa345e4788ad55d73a0c1e35
handrawanw <42910429+handrawanw@users.noreply.github.com>
2021-12-15 12:26:30 +0700

    Update dbConnect.js

--- dbConnect.js
+++ dbConnect.js
@@ -2,8 +2,6 @@ var mongoose = require('mongoose');
 
 const dbConnect = () => {
     // const mongoURI = process.env.MONGO_URL;
-    const mongoURI = 'mongodb+srv://handrawan:1FtwSnIrkcSQBwXq@cluster0.kaius.mongodb.net/kangoid?authSource=admin&replicaSet=atlas-da52ap-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
-    // const mongoURI = 'mongodb://localhost:27017/purchasing_order';
     mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify:false,useCreateIndex:true, connectTimeoutMS: 15000 });
     let db = mongoose.connection;
     db.on('error', console.log.bind(console, 'connection error:'));
